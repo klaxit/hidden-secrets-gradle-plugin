29ed759eb842c6496ff22dcc2b94479d71542717
Benjamin AIMONE <benjamin.aimone@gmail.com>
2020-11-30 16:14:01 +0100

    Update the test sdk from old kotlintest to kotest and add some tests

--- src/test/kotlin/HiddenSecretsTest.kt
+++ src/test/kotlin/HiddenSecretsTest.kt
@@ -1,7 +1,12 @@
-import io.kotlintest.specs.WordSpec
+import com.klaxit.hiddensecrets.HiddenSecretsPlugin
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.matchers.string.shouldContain
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.rules.TemporaryFolder
 
+/**
+ * Test that HiddenSecrets commands are working.
+ */
 class HiddenSecretsTest : WordSpec({
 
     "Apply the plugin" should {
@@ -11,16 +16,41 @@ class HiddenSecretsTest : WordSpec({
         buildFile.appendText("""
         plugins {
             id 'com.klaxit.hiddensecrets'
+            id 'com.android.application'
+        }
+        android {
+            compileSdkVersion 29
         }
         """.trimIndent())
         val gradleRunner = GradleRunner.create()
-                .withPluginClasspath()
-                .withProjectDir(testProjectDir.root)
-                .withTestKitDir(testProjectDir.newFolder())
+            .withPluginClasspath()
+            .withProjectDir(testProjectDir.root)
+            .withTestKitDir(testProjectDir.newFolder())
 
-        "Make command copyCpp works" {
-            val result = gradleRunner.withArguments("copyCpp").build()
+        //Properties
+        val key = "thisIsATestKey"
+        val packageName = "com.package.test"
+
+        "Make command ${HiddenSecretsPlugin.TASK_COPY_CPP} succeed" {
+            val result = gradleRunner.withArguments(HiddenSecretsPlugin.TASK_COPY_CPP).build()
             println(result.output)
+        }
+
+        "Make command ${HiddenSecretsPlugin.TASK_COPY_KOTLIN} succeed" {
+            val result = gradleRunner.withArguments(HiddenSecretsPlugin.TASK_COPY_KOTLIN, "-Ppackage=$packageName").build()
+            println(result.output)
+        }
+
+        "Make command ${HiddenSecretsPlugin.TASK_OBFUSCATE} succeed" {
+            val result = gradleRunner.withArguments(HiddenSecretsPlugin.TASK_OBFUSCATE, "-Pkey=$key", "-Ppackage=$packageName").build()
+            println(result.output)
+            result.output shouldContain "{ 0x15, 0x58, 0xb, 0x43, 0x78, 0x4a, 0x23, 0x6d, 0x1, 0x4b, 0x46, 0x7c, 0x57, 0x41 }" //obfuscated key
+        }
+
+        "Make command ${HiddenSecretsPlugin.TASK_PACKAGE_NAME} succeed" {
+            val result = gradleRunner.withArguments(HiddenSecretsPlugin.TASK_PACKAGE_NAME, "-Ppackage=$packageName").build()
+            println(result.output)
+            result.output shouldContain packageName
         }
     }
 })
